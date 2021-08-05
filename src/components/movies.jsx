3c41c185cac132200a7427e4acea82c066deefca
chenyongping <chenyongping001@gmail.com>
2021-08-05 17:15:17 +0800

    Extracting MoviesTable

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import { getGenres } from "../services/fakeGenreService";
 import { getMovies } from "../services/fakeMovieService";
 import { paginate } from "../utils/paginate";
-import Like from "./common/like";
 import ListGroup from "./common/listgroup";
 import Pagination from "./common/pagination";
+import MoviesTable from "./moviesTable";
 class Movies extends Component {
   state = {
     movies: [],
@@ -64,42 +64,11 @@ class Movies extends Component {
         </div>
         <div className="col">
           <p>Showing {count} movies in the database.</p>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">title</th>
-                <th scope="col">genre</th>
-                <th scope="col">Stock</th>
-                <th scope="col">Rate</th>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {movies.map((movie) => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Like
-                      liked={movie.liked}
-                      onLike={() => this.handleLike(movie)}
-                    />
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => this.handleDelete(movie)}
-                      className="btn btn-danger btn-sm"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <MoviesTable
+            movies={movies}
+            onDelete={this.handleDelete}
+            onLike={this.handleLike}
+          />
           <Pagination
             itemsCount={count}
             pageSize={pageSize}
---
+++ src/components/moviesTable.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Like from "./common/like";
+
+const MoviesTable = (props) => {
+  const { movies, onLike, onDelete } = props;
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">title</th>
+          <th scope="col">genre</th>
+          <th scope="col">Stock</th>
+          <th scope="col">Rate</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {movies.map((movie) => (
+          <tr key={movie._id}>
+            <td>{movie.title}</td>
+            <td>{movie.genre.name}</td>
+            <td>{movie.numberInStock}</td>
+            <td>{movie.dailyRentalRate}</td>
+            <td>
+              <Like liked={movie.liked} onLike={() => onLike(movie)} />
+            </td>
+            <td>
+              <button
+                onClick={() => onDelete(movie)}
+                className="btn btn-danger btn-sm"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default MoviesTable;
