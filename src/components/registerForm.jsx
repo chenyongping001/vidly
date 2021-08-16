d9866aa99205d6ce6820ef9ce73d1fbef0b005b6
chenyongping <chenyongping001@gmail.com>
2021-08-16 21:19:00 +0800

    commit register Form

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import * as userService from "../services/userService";
 
 class RegisterForm extends Form {
   schema = {
@@ -14,9 +15,9 @@ class RegisterForm extends Form {
     errors: {},
   };
 
-  doSubmit() {
-    console.log("submitted.");
-  }
+  doSubmit = async () => {
+    await userService.register(this.state.data);
+  };
 
   render() {
     return (
